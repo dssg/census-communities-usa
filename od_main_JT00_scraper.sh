71df1c65ebb4768b551fcdfe7aa411cbe47dfd54
fishmanadam <fishman.adam@gmail.com>
2013-07-17 18:58:36 -0500

    Update od_main_JT00_scraper.sh

--- od_main_JT00_scraper.sh
+++ od_main_JT00_scraper.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for i in `seq 2002 2011`;
+
+declare -a arr=(al ak az ar ca co ct de dc fl ga hi id il in ia ks ky la me md ma mi mn ms mo mt ne nv nh nj nm ny nc nd oh ok or pa ri sc sd tn tx ut vt va wa wv wi wy)
+for j in ${arr[@]}
 do
-  wget http://lehd.did.census.gov/onthemap/LODES7/al/od/al_od_main_JT00_ + | gzip -d
-  
+for i in 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011
+do
+beginning="http://lehd.did.census.gov/onthemap/LODES7/$j/od/$j"
+end="_od_main_JT00_$i.csv.gz"
+wget $beginning$end | gzip -d >> states.csv
+done
+done
