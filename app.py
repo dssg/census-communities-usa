16537f3b004f325ee1f6a7420e5b622b24f22aea
Hunter Owens <owens.hunter@gmail.com>
2013-07-17 20:02:16 -0500

    sercet heroku mongo jankiness

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 import simplejson as json
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return os.environ.get('MONGOHQ_URL')
 
 if __name__ == "__main__":
     app.run(debug=True)
