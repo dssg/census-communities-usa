aab9a46541427d3ecade9ec31d2b07f63b24a61a
fishmanadam <fishman.adam@gmail.com>
2013-07-17 19:06:02 -0500

    finished!

--- od_main_JT00_scraper.sh
+++ od_main_JT00_scraper.sh
@@ -7,6 +7,9 @@ for i in 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011
 do
 beginning="http://lehd.did.census.gov/onthemap/LODES7/$j/od/$j"
 end="_od_main_JT00_$i.csv.gz"
-wget $beginning$end | gzip -d >> states.csv
+end2="_od_main_JT00_$i.csv"
+wget $beginning$end
+gzip -d $j$end
+cat $j$end2 >> states.csv
 done
 done
