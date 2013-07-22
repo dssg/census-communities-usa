a172ff8619d8486d2ad6dbdc062aaee66b5f2507
Hunter Owens <owens.hunter@gmail.com>
2013-07-22 18:31:10 -0500

    examples

--- app.py
+++ app.py
@@ -10,28 +10,39 @@ from bson.json_util import dumps
 
 app = Flask(__name__)
 
-#BAD MONGO URL
-#NEVER DO THIS
+
 
 @app.route("/state/<state>")
 def get_state(state):
-	return placeholder
+	return "Not Yet Implemented"
 
 @app.route("/year/<year>")
 def get_year(year):
-	return "you wanted a year"
+	return "Not Yet Implemented"
 
 @app.route("/get-all/<year>/<state>")
 def get_year_and_state(year,state):
-	return str(os.environ.get('MONGOHQ_URL'))
+	return "Not Yet Implemented"
 
 @app.route("/residence-block/<int:block>")
 def find_block(block):
-	return "test"
+	block_records = []
+	collection = db.census
+	for record in collection.find({"Residence_Census_Block_Code":block})[20]:
+		block_records.append(record)
+	return dumps(block_records) 
+
+@app.route("/demo")
+def demo():
+	collection = db.census
+	return dumps(collection.fine_one())
 
 @app.route("/")
 def hello():
     return "Hello World, again"
 
 if __name__ == "__main__":
+	mongo_url = str(os.environ.get('MONGO_URL'))
+	client = pymongo.MongoClient(mongo_url,27017)
+	db = client.census
 	app.run(debug=True)
