3809c6a82ca3ee8cb9b0feec1950cb8928b0972c
Eric van Zanten <eric@bahai.us>
2013-08-03 08:22:43 -0500

    Added Alaska to the states file. Improved import script to be better at picking up in the middle. Added init.py to scrapers folder so it could be used like a module

--- scrapers/fetch_and_load.py
+++ scrapers/fetch_and_load.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import ConnectionError
 import os
 import pymongo
 import gzip
@@ -46,15 +47,29 @@ def fetch_load_xwalk(state):
             coll.insert([r for r in group if r])
         coll.ensure_index([('stusps', pymongo.DESCENDING)])
 
-def fetch_load(year, state):
-    for group in ['od', 'rac', 'wac']:
+def fetch_load(year, state, attempts=0, **kwargs):
+    groups = ['od', 'rac', 'wac']
+    job_types = ['JT00', 'JT01', 'JT02', 'JT03', 'JT04', 'JT05']
+    if kwargs.get('groups') and if 'all' not in kwargs.get('group'):
+        groups = kwargs.get('groups')
+    if kwargs.get('job_types') and if 'all' not in kwargs.get('job_types'):
+        job_types = kwargs.get('job_types')
+    for group in groups:
         coll = MONGO_DB[COLLS[group]]
-        for segment in SEGMENTS[group]:
-            for job_type in ['JT00', 'JT01', 'JT02', 'JT03', 'JT04', 'JT05']:
+        if not kwargs.get('segments') or if 'all' in kwargs.get('segments'):
+            segments = SEGMENTS[group]
+        else:
+            segments = kwargs.get('segments')
+        for segment in segments:
+            for job_type in job_types:
+                state = state.lower()
                 u = '%s/%s/%s/%s_%s_%s_%s_%s.csv.gz' % (ENDPOINT, state, group, state, group, segment, job_type, year)
-                req = requests.get(u)
+                try:
+                    req = requests.get(u)
+                except ConnectionError:
+                    return 'Was unable to load %s' % u
                 if req.status_code != 200:
-                    print 'No %s data for %s in the year %s of type %s' % (group, state, year, job_type)
+                    print 'No %s data for segment %s in %s in the year %s of type %s' % (group, segmenet, state, year, job_type)
                     continue
                 s = StringIO(req.content)
                 with gzip.GzipFile(fileobj=s) as f:
@@ -67,17 +82,57 @@ def fetch_load(year, state):
                                 row['stusps'] = state.upper()
                                 rows.append(row)
                         coll.insert(rows)
+                print 'Successfully loaded %s' % u
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 2:
-        states = [sys.argv[1]]
-    else:
-        states_file = open('50state.txt', 'rb')
-        states = [s[:2].lower() for s in states_file]
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--states', type=str, required=True,
+        help="""
+            Comma separated list of two letter USPS abbreviation for the state 
+            you want to load the data for. Provide 'all' to load all states""")
+    parser.add_argument('--files', type=str, required=True,
+        help="""
+            Comma separated list of files you would like to load. Valid values include:
+            'od', 'rac','wac' and geo_xwalk. Provide 'all' to load all four.
+        """)
+    parser.add_argument('--segments', type=str, required=True,
+        help="""
+          Comma separated list of workforce segments you want to load. In the 
+          case of Origin Destination data, this translates into the part (either 'main' or 'aux').
+          Provide 'all' to load all segments.
+          """)
+    parser.add_argument('--job_types', type=str, required=True,
+        help="""
+            Comma separated list of job types you would like to load.
+            Provie 'all' to load all job types.
+        """)
+    parser.add_argument('--years', type=str, required=True,
+        help="""
+            Comma separated list of years you would like to load.
+            Provie 'all' to load all years between 2002 and 2011.
+        """)
+    args = parser.parse_args()
+    states_file = open('50state.txt', 'rb')
+    states_list = [s[:2] for s in states_file]
+    states = args.states.split(',')
+    if 'all' in states:
+        states = states_list
+    if not set(states).issubset(set(states_list)):
+        return 'The list of states you provided included an invalid value: %s' % args.states
+    years = args.years.split(',')
+    if 'all' in years:
+        years = range(2002, 2012)
+    kwargs = {
+        'groups': args.files.split(','),
+        'job_types': args.job_types.split(','),
+        'segments': args.segments.split(',')
+    }
     for state in states:
         print 'Loading geographic crosswalk table for %s' % state.upper()
         fetch_load_xwalk(state)
-        for year in range(2002, 2012):
+        for year in years:
             print 'Loading data from %s for %s' % (year, state.upper())
             fetch_load(year, state)
