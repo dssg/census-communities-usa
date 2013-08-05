c1bedb3d5f99327787a4054a99701fc13356418e
Eric van Zanten <eric.vanzanten@gmail.com>
2013-08-05 07:49:47 -0500

    Fixed one last bug

--- scrapers/fetch_and_load.py
+++ scrapers/fetch_and_load.py
@@ -137,7 +137,8 @@ def fetch_load(year, state, **kwargs):
                                     row['work_st_leg_upper_code'] = work_geo_xwalk['stsldu']
                                     row['work_st_leg_upper_name'] = work_geo_xwalk['stslduname']
                                 rows.append(row)
-                        make_indexes(group, coll, row)
+                        if row:
+                            make_indexes(group, coll, row)
                         coll.insert(rows)
                 print 'Successfully loaded %s' % u
 
