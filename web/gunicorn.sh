7579d834359c05324fde0358a88ad64b2383cfba
Eric van Zanten <eric.vanzanten@gmail.com>
2013-08-29 06:02:45 -0500

    Increased the timeout on the gunicorn init script

--- web/gunicorn.sh
+++ web/gunicorn.sh
@@ -12,4 +12,4 @@ GROUP=evz
 cd $PROJECT/web
 source $VENV/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec $VENV/bin/gunicorn -w $NUM_WORKERS --bind 127.0.0.1:7777 --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE app:app
+exec $VENV/bin/gunicorn -w $NUM_WORKERS --bind 127.0.0.1:7777 --user=$USER --group=$GROUP --log-level=debug --log-file=$LOGFILE 2>>$LOGFILE app:app -t 60
