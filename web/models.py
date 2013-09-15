d44d3203b3bb884802d482e3338be0180baa1659
Hunter Owens <owens.hunter@gmail.com>
2013-09-15 15:25:46 -0500

    compisite primary keys. #25

--- web/models.py
+++ web/models.py
@@ -12,8 +12,8 @@ db = SQLAlchemy(app)
 
 class OriginDestination(db.Model):
 	__tablename__ = 'origin_destination'
-	h_geocode 	= db.Column(db.String(15),nullable=False)
-	w_geocode 	= db.Column(db.String(15),nullable=False)
+	h_geocode 	= db.Column(db.String(15),nullable=False,primary_key=True)
+	w_geocode 	= db.Column(db.String(15),nullable=False,primary_key=True)
  	s000      	= db.Column(db.Integer) 
  	sa01      	= db.Column(db.Integer) 
  	sa02      	= db.Column(db.Integer) 
@@ -25,7 +25,7 @@ class OriginDestination(db.Model):
  	si02	  	= db.Column(db.Integer) 
  	si03	  	= db.Column(db.Integer)  
  	createdate 	= db.Column(db.Date)    
- 	data_year 	= db.Column(db.Integer, nullable=False) 
- 	job_type  	= db.Column(db.String(255), nullable=False)
+ 	data_year 	= db.Column(db.Integer, nullable=False,primary_key=True) 
+ 	job_type  	= db.Column(db.String(255), nullable=False,primary_key=True)
 
 print OriginDestination.query.limit(1).all()
